3b4638e2455db729b7aa72aaf3f365c642f63e9e
imoitra007 <email.indradeep@gmail.com>
2018-08-25 23:56:17 +0530

    Forget Password UI Created

--- my-dream-app/src/app/Login/login.component.ts
+++ my-dream-app/src/app/Login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  showForgetPasswordDiv : Boolean = false;
+  showLoginDiv : Boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // This function will be called when we click on "Forget Password" link in UI.
+  onClickForgetPassword(){
+    this.showForgetPasswordDiv = true;
+    this.showLoginDiv = false;
+  }
+
+  // This function will be called when we click on "Cancel" button in Forget Password Component.
+  onCancelForgetPassword(){
+    this.showForgetPasswordDiv = false;
+    this.showLoginDiv = true;
   }
 
 }
