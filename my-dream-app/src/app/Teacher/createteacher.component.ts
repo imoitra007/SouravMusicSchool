89de4a699110422cac465ec24749c3b8613f8d7f
imoitra007 <42399766+imoitra007@users.noreply.github.com>
2018-08-27 19:00:07 +0530

    Teacher UI Created

---
+++ my-dream-app/src/app/Teacher/createteacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createteacher',
+  templateUrl: './createteacher.component.html',
+  styleUrls: ['./createteacher.component.css']
+})
+export class CreateteacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { CreateteacherComponent } from './Teacher/createteacher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     LoginComponent,
-    ForgetpasswordComponent
+    ForgetpasswordComponent,
+    CreateteacherComponent
   ],
   imports: [
     BrowserModule
